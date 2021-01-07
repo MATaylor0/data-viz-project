8a067407090ac1916d0920c633682fe9424afd2a
Kenny Dao <kennydao369@gmail.com>
2021-01-07 20:33:14 +1100

    update logic.js

--- KD/static/js/logic.js
+++ KD/static/js/logic.js
@@ -1,97 +1,93 @@
 // loading geoJSON from source
-
-var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-
 // Initialize map, setting the streetmap and earthquakes layers to display on load
-// Set view to san francisco latlng
-var mymap = L.map("map", {center: [37.73379, -122.44676], zoom: 3});
-
-
-
+// Set view to Central Europe latlng
+var mymap = L.map("map", {center: [50.378472, 14.970598], zoom: 3});
 
 // Use Thunderforest.Outdoors layer as basemap
 var lyrOutdoors = L.tileLayer.provider('Thunderforest.Outdoors');
+
 mymap.addLayer(lyrOutdoors);
 
-// const geoData = 'C:/Users/home/Documents/data-viz-project/KD/static/data/countries.geojson'
+const geoData = './static/data/countries.geojson';
 
-// layer boundaries
+// declare variables
+var objBasemaps;
+var objOverlays;
+var ctlLayers;
 var lyrBoundaries;
-lyrBoundaries = new L.LayerGroup();
 
-var geoData = './static/data/countries.geojson';
+var ctyData ={};
+var expVal;
+var att;
 
-var geojson;
+d3.csv('./static/data/export_data_2018.csv', function(expData) {
 
-// Grab data with d3
-d3.json(geoData, function(data) {
-    console.log(data);
-    // Create a new choropleth layer
-    geojson = L.choropleth(data, {
+  // console.log(expData);
 
-      // Define what  property in the features to use
-    //   valueProperty: "geometry",
-      // Set color scale
-    //   scale: ["#ffffb2", "#e600e6"],
+  for (var i = 0; i < expData.length; i++) {
+    var att = expData[i]
+    // console.log(att);
+    ctyData[att.location_code] = att.export_value;
 
-      // Number of breaks in step range
-    //   steps: 10,
 
-      // q for quartile, e for equidistant, k for k-means
-      mode: "q",
-      style: {
-        // Border color
-        color: getColor(data.export_vals),
-        weight: 1,
-        fillOpacity: 0.6
-      },
-
-      // Binding a pop-up to each layer
-    //   onEachFeature: function(feature, layer) {
-    //     layer.bindPopup("Zip Code: " + feature.properties.ZIP + "<br>Median Household Income:<br>" +
-    //       "$" + feature.properties.MHI2016);
-    //   }
-    }).addTo(mymap);
+  }
+  // rowData["country_code"] = d.location_code;
+  // rowData["country_name"] = d.country_name;
+  // rowData["export_value"] = d.export_value;
+  // rowData["import_value"] = d.import_value;
 });
 
-// use d3 to load countries.geojson data
-/*d3.json(geoData, function(json) {
-    console.log(json);
-    // adding boundaries to layer
-    L.geoJSON(json, {color: '#424a44', weight:1.1, fillColor:'#004691', Opacity: 1, dashArray: '1'}).addTo(lyrBoundaries);
-    lyrBoundaries.addTo(mymap);
+lyrBoundaries = new L.LayerGroup();
+lyrBoundaries = L.geoJSON.ajax('./static/data/countries.geojson',{ style: myStyle,
+                                  onEachFeature: function(feature, layer) {
+                                  layer.bindPopup("<h5>" + feature.properties.ADMIN + "</h5><hr>" + "export value: " + parseFloat(optColor).toFixed(2));
+                                }
+  });
 
+function myStyle(feature) {
 
-})
-*/
-function CtyMarkers(json, latlng) {
-    console.log(json);
-    optColor = json.properties.mag;
+  if (feature.properties.ISO_A3 != "-99") {
+    optColor = ctyData[feature.properties.ISO_A3];
+    console.log(optColor);
 
-    // create circle marker for each feature
-    var myMarkers = L.circleMarker(latlng, {radius:optColor*6, color:'black',
-                                fillColor:getColor(optColor),
-                                weight: 0.3, fillOpacity:0.8});
+    return {
+        fillColor: getColor(optColor),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+  }
+  else {
+    console.log('ERROR: -99 found')
+    return
+  }
 
-    // set popup label on mouse click
-    myMarkers.bindPopup("<h5>" + json.properties.place + "</h5><hr>"
-                         + new Date(json.properties.time) + " - Magnitude: " + optColor,{interactive:true});
+// add layer to map
+lyrBoundaries.addTo(mymap);
 
-    return myMarkers;
+// function to assign suitable color depend up export value
+function getColor(val) {
+
+  // console.log('getColor was called!', val);
+  // use conditional operator (?:) to return suitable color scheme
+  return  val < 1000000000 ? '#86b300' :
+          val < 150000000000 ? '#ffcc66':
+          val < 200000000000 ? '#ff9900':
+          val < 250000000000 ? '#e68a00':
+          val < 300000000000 ? '#b36b00':
+          val < 350000000000 ? '#ff0000':
+          val < 350000000000 ? '#00ffff':
+          val < 400000000000 ? '#00bfff':
+          val < 450000000000 ? '#0080ff':
+          val < 500000000000 ? '#0040ff':
+          val < 550000000000 ? '#8000ff':
+          val < 600000000000 ? '#00661a':
+          val < 650000000000 ? '#006666':
+                               '#000066';
 }
 
-function getColor(d) {
-
-    // use conditional operator (?:) to return suitable color scheme
-    return d < 1 ? '#ecffb3' :
-           d < 2 ? '#ffcc66' :
-           d < 3 ? '#ff9900' :
-           d < 4 ? '#e68a00' :
-           d < 5 ? '#b36b00' :
-           d >= 5 ?  '#ff0000' :
-                    '#ff0000';
-}
-/*
 // define basemaps
 objBasemaps = {
     "Outdoors": lyrOutdoors
@@ -102,5 +98,4 @@ objOverlays = {
     "Boundary":lyrBoundaries
 };
 
-ctlLayers = L.control.layers(objBasemaps, objOverlays, {collapsed: false}).addTo(mymap);
-*/
+// ctlLayers = L.control.layers(objBasemaps, objOverlays, {collapsed: false}).addTo(mymap);
