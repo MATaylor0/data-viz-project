4287ebcbe36eface6178f9c845b0d96eceb0d63a
Kenny Dao <kennydao369@gmail.com>
2021-01-11 09:31:11 +1100

    update logic.js and index.html

--- KD/static/js/logic.js
+++ KD/static/js/logic.js
@@ -1,14 +1,19 @@
 // loading geoJSON from source
 // Initialize map, setting the streetmap and earthquakes layers to display on load
 // Set view to Central Europe latlng
-var mymap = L.map("map", {center: [50.378472, 14.970598], zoom: 3});
+var mymap = L.map("map", {center: [50.378472, 14.970598],
+                          zoom: 2, maxZoom: 2.5, minZoom:2});
+
+// set max bounds for the map
+mymap.setMaxBounds(mymap.getBounds().pad(0.1));
+// mymap.setZoom(5)
 
 // Use Thunderforest.Outdoors layer as basemap
 var lyrOutdoors = L.tileLayer.provider('Thunderforest.Outdoors');
 
 mymap.addLayer(lyrOutdoors);
 
-const geoData = './static/data/countries.geojson';
+const geoQuery = './static/data/countries.geojson';
 
 // declare variables
 var objBasemaps;
@@ -22,73 +27,72 @@ var expVal, impVal, defVal;
 var selYear;
 var optColor;
 
+
+// lyrBoundaries = new L.LayerGroup();
+
 // use d3 to load csv file and create dictionary to hold key/value pairs
 d3.csv('./static/data/export_data_2018.csv', function(expData) {
 
-  // console.log(expData);
-  expData.export_value = +expData.export_value;
-  expData.import_value = +expData.import_value;
+  // parsing data
+  parseData(expData);
 
-  for (var i = 0; i < expData.length; i++) {
-    var att = expData[i];
-    expDict[att.location_code] = att.export_value;
-    impDict[att.location_code] = att.import_value;
+  // set tooltip for each country
+  setTooltip();
 
-  }
+  // add layer to map
+  lyrBoundaries.addTo(mymap);
+
 });
 
-// setup bindPopup for each country
-lyrBoundaries = new L.LayerGroup();
-
-// initialize the layer and tooltip after the basemap loaded
-setTooltip();
-
-// setting tooltip for layerBoundaries
+// function setting tooltip for layerBoundaries
 function setTooltip() {
-  lyrBoundaries = L.geoJSON.ajax('./static/data/countries.geojson',{ style: myStyle,
-                                  onEachFeature: function(feature, layer) {
 
-                                  // console.log(selYear);
-                                  var expVal = parseFloat(optColor).toFixed(2);
-                                  var impNum = parseFloat(impVal).toFixed(2);
+  lyrBoundaries = L.geoJSON.ajax(geoQuery, { style: myStyle,
+                    onEachFeature: function(feature, layer) {
 
-                                  var trdDefi = expVal - impNum;
-                                  trdDefi.toFixed(0);
-                                  expVal = numberWithCommas(expVal);
-                                  impNum = numberWithCommas(impNum);
+                    // console.log(selYear);
+                    var expVal = parseFloat(optColor).toFixed(2);
+                    var impNum = parseFloat(impVal).toFixed(2);
 
-                                  layer.bindTooltip("<h4 style = 'text-align: center; background-color: #ffcc66'><b>" + feature.properties.ADMIN + "</h4></b>" +
-                                    "Export: " + '$' + expVal + '<br>' + 'Import: ' + '$' + impNum + '<br>' +
-                                    'Surplus: ' + (trdDefi < 0 ? '-' + formatDollar(trdDefi) : formatDollar(trdDefi)),{interactive:false});
-                                  layer.on({
-                                    mouseover: highlightFeature,
-                                    mouseout: resetHighlight
-                                  });
-                                }
+                    // check if the data is available
+                    if (isNaN(expVal)) {
+                      layer.bindTooltip("<h4 style = 'text-align: center; background-color: #ffcc66'><b>" +
+                      feature.properties.ADMIN + "</h4></b>" + "Data unavailable!");
+                    }
+
+                    else {
+                    var trdDefi = expVal - impNum;
+                    trdDefi.toFixed(2);
+                    expVal = numberWithCommas(expVal);
+                    impNum = numberWithCommas(impNum);
+
+                    layer.bindTooltip("<h4 style = 'text-align: center; background-color: #ffcc66'><b>" +
+                      feature.properties.ADMIN + "</h4></b>" + "Export: " + '$' + expVal + '<br>' + 'Import: ' + '$' + impNum + '<br>' +
+                      'Surplus: ' + (trdDefi < 0 ? '-' + formatDollar(trdDefi) : formatDollar(trdDefi)),{interactive:false});
+                    }
+                    layer.on({
+                      mouseover: highlightFeature,
+                      mouseout: resetHighlight
+                    });
+                  }
   });
 }
 
-// add layer to map
-lyrBoundaries.addTo(mymap);
-
 // use jQuery library to acquire the selected year in radio button
 $("input[name=fltYear]").click(function(){
-  selYear = $("input[name=fltYear]:checked").val()
+  var selYear = $("input[name=fltYear]:checked").val();
   // console.log(selYear);
+
+  expDict = {};
+  impDict = {};
 
   switch(selYear) {
     case '2016':
-      console.log(selYear);
+
       d3.csv('./static/data/export_data_2016.csv', function(expData) {
 
-        expData.export_value = +expData.export_value;
-        expData.import_value = +expData.import_value;
+        parseData(expData);
 
-        for (var i = 0; i < expData.length; i++) {
-          var att = expData[i];
-          expDict[att.location_code] = att.export_value;
-          impDict[att.location_code] = att.import_value;
-        }
       });
 
       setTooltip();
@@ -97,14 +101,9 @@ $("input[name=fltYear]").click(function(){
     case '2017':
 
       d3.csv('./static/data/export_data_2017.csv', function(expData) {
-        // console.log(expData);
-        expData.export_value = +expData.export_value;
-        expData.import_value = +expData.import_value;
-        for (var i = 0; i < expData.length; i++) {
-          var att = expData[i];
-          expDict[att.location_code] = att.export_value;
-          impDict[att.location_code] = att.import_value;
-        }
+
+        parseData(expData);
+
       });
 
       setTooltip();
@@ -113,31 +112,38 @@ $("input[name=fltYear]").click(function(){
     case '2018':
 
       d3.csv('./static/data/export_data_2018.csv', function(expData) {
-        // console.log(expData);
-        expData.export_value = +expData.export_value;
-        expData.import_value = +expData.import_value;
-        for (var i = 0; i < expData.length; i++) {
-          var att = expData[i];
-          expDict[att.location_code] = att.export_value;
-          impDict[att.location_code] = att.import_value;
-        }
+
+        parseData(expData);
       });
-      console.log(expDict);
+
       setTooltip();
       break;
+
   }
+  // refresh layer boundaries with respective tooltip data to map
   lyrBoundaries.addTo(mymap);
+
 });
 
-// filter out the outlier data points
-// // lyrBoundaries.refilter(function(feature){
-// //   return feature.properties.key != "-99";
-// });
+// function to parse data from csv file
+function parseData(expData) {
+
+  expData.export_value = +expData.export_value;
+  expData.import_value = +expData.import_value;
+
+  // looping through each row to assign export/import value to dictionaries
+  for (var i = 0; i < expData.length; i++) {
+    var att = expData[i];
+    expDict[att.location_code] = att.export_value;
+    impDict[att.location_code] = att.import_value;
+  }
+};
 
 // function style
 function myStyle(feature) {
-
+  // mark down the outlier data points
   if (feature.properties.ISO_A3 != "-99") {
+
     optColor = expDict[feature.properties.ISO_A3];
     impVal = impDict[feature.properties.ISO_A3];
     // console.log(optColor);
@@ -145,15 +151,19 @@ function myStyle(feature) {
     return {
         fillColor: getColor(optColor),
         weight: 1,
-        // opacity: 1,
         color: 'white',
         dashArray: '3',
         fillOpacity: 0.7
     };
   }
   else {
-    // console.log('ERROR: -99 found!')
-    return
+    return {
+    fillColor: getColor(optColor),
+        weight: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
   }
 }
 
@@ -176,7 +186,6 @@ function highlightFeature(e) {
 // function reset highlight
 function resetHighlight(e) {
   lyrBoundaries.resetStyle(e.target);
-
 }
 
 // function to assign suitable color depend up export value
@@ -210,13 +219,11 @@ function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-
 // Set up the legend
 
 var legend = L.control({position: 'bottomleft'});
 
   legend.onAdd = function () {
-
     var div = L.DomUtil.create('div', 'info legend');
     var lgdInfo = [1000000, 10000000, 100000000,
                   1000000000, 10000000000, 100000000000,
@@ -235,22 +242,7 @@ var legend = L.control({position: 'bottomleft'});
 legend.addTo(mymap);
 
 mymap.on('click', function(e){
-  if (e.originalEvent.shiftKey) {
-      alert(mymap.getZoom());
-  } else {
-      alert(e.latlng.toString());
-  }
+
+  alert(e.latlng.toString());
+
 });
-
-// define basemaps
-objBasemaps = {
-  "Outdoors": lyrOutdoors
-};
-
-// define overlays
-objOverlays = {
-  "Boundary":lyrBoundaries
-};
-
-
-// ctlLayers = L.control.layers(objBasemaps, objOverlays, {collapsed: false}).addTo(mymap);
